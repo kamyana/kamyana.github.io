395981f4d7c4ab3c957e26aa33bf34f563177171
kamyana <32121219+kamyana@users.noreply.github.com>
2017-09-20 12:09:26 +0300

    Add weekend data

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ var uaDay; //день тижня українською
 var uaMonth; //місяць українською
 var myDay; //чи робочий чи вихідний
 var rkam = [390, 410, 430, 450, 470, 490, 510, 530, 550, 570, 600, 630, 660, 710, 750, 780, 830, 870, 900, 930, 960, 990, 1020, 1050, 1080, 1110, 1140, 1200]; //на Камяну в хвилинах
-var busKam = [2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 3]; //автобус на Камяну, кожному автобусу відповідає номер в хвилинах
+var rbusKam = [2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 3]; //автобус на Камяну, кожному автобусу відповідає номер в хвилинах
 var rcher = [390, 410, 430, 450, 470, 490, 510, 540, 570, 600, 640, 680, 720, 760, 800, 840, 870, 900, 930, 960, 990, 1020, 1050, 1080, 1110, 1140, 1170, 1215, 1260]; //на Чернівці в хвилинах
-var busCher = [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 3]; //автобус на Чернівці, кожному автобусу відповідає номер в хвилинах
+var rbusCher = [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 3]; //автобус на Чернівці, кожному автобусу відповідає номер в хвилинах
+var vkam = [420, 450, 480, 510, 540, 580, 620, 660, 700, 740, 780, 820, 860, 900, 940, 980, 1020, 1060, 1100, 1140, 1180, 1230];
+var vbusKam = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2];
+var vcher = [390, 420, 450, 480, 510, 540, 580, 620, 660, 700, 740, 780, 820, 860, 900, 940, 980, 1020, 1060, 1100, 1140, 1180];
+var vbusCher = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2];
 var h = date.getHours(); //поточна година
 var m = date.getMinutes(); //поточна хвилина
 var	s = date.getSeconds(); //поточні секунди
@@ -57,22 +61,40 @@ function showTime (mnbegin,buses)
 		}
 	}
 	var eachTT = function()
-	{
-			showTime(rkam,busKam);
+	{ if (myDay == 0)
+		{
+			showTime(rkam,rbusKam);
 			if (bus==undefined)
 			{document.getElementById("p1").innerHTML = "Автобус № 2 на Кам'яну відправиться о 6:30";}
 			else
-			{document.getElementById("p1").innerHTML = "Автобус № " + bus + " на Кам'яну відправиться о " + mnCount + ', через ' + mnforarrive + ' хвилин.';}
-				showTime(rcher,busCher);
+			{
+				document.getElementById("p1").innerHTML = "Автобус № " + bus + " на Кам'яну відправиться о " + mnCount + ', через ' + mnforarrive + ' хвилин.';}
+				showTime(rcher,rbusCher);
 				if (bus==undefined)
 				{document.getElementById("p2").innerHTML = "Автобус № 1 на Чернівці відправиться о 6:30";}
 				else
 				{document.getElementById("p2").innerHTML = "Автобус № " + bus + " на Чернівці відправиться о " + mnCount + ', через ' + mnforarrive + ' хвилин.';}
+			}
+			else {if (myDay == 1)
+				{
+					showTime(vkam,vbusKam);
+					if (bus==undefined)
+					{document.getElementById("p1").innerHTML = "Автобус № 1 на Кам'яну відправиться о 6:30";}
+					else
+					{document.getElementById("p1").innerHTML = "Автобус № " + bus + " на Кам'яну відправиться о " + mnCount + ', через ' + mnforarrive + ' хвилин.';}
+						showTime(vcher,vbusCher);
+						if (bus==undefined)
+						{document.getElementById("p2").innerHTML = "Автобус № 1 на Чернівці відправиться о 7:00";}
+						else
+						{document.getElementById("p2").innerHTML = "Автобус № " + bus + " на Чернівці відправиться о " + mnCount + ', через ' + mnforarrive + ' хвилин.';}
+					}
+		}
 	}
-	eachTT();
-	setInterval(eachTT, 10000);
-	document.getElementById("a1").innerHTML = 'Сьогодні ' + nowDay + ' ' + uaMonth + ', ' + uaDay + '.';
-	var ttime = function () {
+			eachTT();
+			setInterval(eachTT, 10000);
+			document.getElementById("a1").innerHTML = 'Сьогодні ' + nowDay + ' ' + uaMonth + ', ' + uaDay + '.';
+	var ttime = function ()
+	{
 		date = new Date();
 		h = date.getHours();
 		m = date.getMinutes();
